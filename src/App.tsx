9dca897c5c62a0cd0dfefeb47714819580b9b6e5
weslleson22 <wesllesonsowza@gmail.com>
2020-06-28 15:06:13 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import './App.css'
 
-import './App.css';
-
-function App() {
-  return (
- 
-      <h1>ok ok</h1>
-        
-
+import Header from './Header';
+function App(){
+  return(
+    <div>
+      <Header/>
+      <h1>Conteúdo da aplicação</h1>
+    </div>
   );
 }
-
 export default App;
