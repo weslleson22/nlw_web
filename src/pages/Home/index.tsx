e1108883faf19974ab55878c3f28638edbbc45b8
weslleson22 <wesllesonsowza@gmail.com>
2020-06-28 20:08:18 -0300

    inidex - do points

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import {FiLogIn} from 'react-icons/fi';
-import logo from '../../assets/logo.svg';
 import './styles.css';
-    
-const Home = ()=>{
-    return(
-      <div id="page-home">
-            <div className="content">
-            <header>
-            <img src={logo} alt="Ecoleta"/>
-            </header>
-            <main>
-                <h1>Seu  marketplace de coleta de resíduos.</h1>
-                <p>Ajudamos pessoas a encontratem pontos de coleta de forma eficiente.</p>
-                <a href="/cadastro">
-                    <span>
-                        <FiLogIn/>
-                    </span>
-                    <strong>Cadastre um ponto de coleta</strong>
-                </a>
-            </main>
-            </div>
-       </div>
-    )
-  }
+import { FiLogIn } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Ecoleta" />
+        </header>
+
+        <main>
+          <h1>Seu market place de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+          <Link to="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>
+              Cadastre um ponto de coleta
+            </strong>
+          </Link>
+        </main>
+      </div>
+    </div>
+  )
+}
 
 export default Home;
