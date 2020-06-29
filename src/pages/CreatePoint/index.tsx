eea072b4341b4ab2de0e52cb5a6cdbf8a81aa775
weslleson22 <wesllesonsowza@gmail.com>
2020-06-29 13:49:09 -0300

    adicionado api do map

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom'; 
 import { FiArrowLeft } from 'react-icons/fi';
+import {Map, TileLayer, Marker} from 'react-leaflet';
 
 import './styles.css';
 
@@ -58,6 +59,15 @@ const CreatePoint = ()=>{
                     <span>Selecione o endereço no mapa</span>
                 </legend>
 
+                    <Map center={[-2.0339365, -45.9705647]}zoom={15}>
+                        // />
+                        <TileLayer
+                                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={[-2.0339365, -45.9705647]}/>
+                    </Map>
+
                     <div className="field-group">
                         <div className = "field">
                             <label htmlFor="uf">Estado (UF)</label>
@@ -114,5 +124,5 @@ const CreatePoint = ()=>{
     </div>
     );
 };
-
+//parei 1:09:55
 export default CreatePoint;
