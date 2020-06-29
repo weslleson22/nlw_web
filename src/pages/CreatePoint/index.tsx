9ff8ea7183ac30f737eac216aafb5fef7864d84a
weslleson22 <wesllesonsowza@gmail.com>
2020-06-29 14:39:40 -0300

    forms items

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom'; 
 import { FiArrowLeft } from 'react-icons/fi';
 import {Map, TileLayer, Marker} from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg'
 
 const CreatePoint = ()=>{
+    const [items, setItems] = useState([]);
+
+    useEffect(()=>{
+        api.get('items').then(response=>{
+            setItems(response.data);
+        })
+    }, []);
+
     return(
         <div id="page-create-point">
             <header>
@@ -91,32 +101,16 @@ const CreatePoint = ()=>{
                     <span>selecione um ou mais itens abaixo</span>
                 </legend>
                 <ul className="items-grid">
-                    <li>
+                    {items.map(item =>(
+                        <li>
                         <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
                         <span>Lampadas</span>
                     </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/baterias.svg" alt="Teste"/>
-                        <span>Baterias</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Teste"/>
-                        <span>Papeis papelão</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/eletronicos.svg" alt="Teste"/>
-                        <span>Eletronicos</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/organicos.svg" alt="Teste"/>
-                        <span>Organicos</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                        <span>Óleo de Cozinha</span>
-                    </li>
+                   
+                    ))}
                 </ul>                
             </fieldset>
+
             <button type="submit">
                 Cadastrar ponto de coleta
             </button>
