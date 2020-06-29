04e75769d2e11999fa2a1974017c519ad7349f37
weslleson22 <wesllesonsowza@gmail.com>
2020-06-29 20:20:23 -0300

    seleção de items

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -30,6 +30,11 @@ const CreatePoint = ()=>{
     const [cities, setCities]= useState<string[]>([]);
 
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+    const[formData, setFormData]=useState({
+        name:  '',
+        email:'',
+        whatsapp:'',
+    });
     
     const [selectdUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity]= useState('0');
@@ -89,9 +94,26 @@ const CreatePoint = ()=>{
     ])
     }
     function handleInputChange(event:ChangeEvent<HTMLInputElement>){ 
+        const{name, value} =event.target;
+
+        setFormData({ ...formData, [name]:value});
 
     }
-    
+    function handleSelectItem(id: number){
+        const alreadySelected = selectedItems.findIndex(item=>item===id);
+        if (alreadySelected>=0){
+            const filteredItems = selectedItems.filter(item=>item!==id);
+
+            setSelectedItems(filteredItems);
+
+        }else{
+            setSelectedItems([ ...selectedItems,id]);
+        }
+       
+    }
+    function handleSubbmit(){
+        
+    }
 
     return(
         <div id="page-create-point">
@@ -195,7 +217,10 @@ const CreatePoint = ()=>{
                 </legend>
                 <ul className="items-grid">
                     {items.map(item =>(
-                        <li key={item.id}>
+                        <li
+                         key={item.id}
+                          onClick= { () => handleSelectItem(item.id)}
+                          className={selectedItems.includes(item.id)?'selected':''}>
                         <img src={item.image_url} alt={item.title}/>
                     <span>{item.title}</span>
                     </li>
