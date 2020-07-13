ac5b1923b398517dfcc6ee3a53438cee83c67d02
weslleson22 <wesllesonsowza@gmail.com>
2020-07-13 18:01:53 -0300

    versão ok

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -6,10 +6,16 @@ import axios from 'axios';
 import {LeafletMouseEvent, LeafletEvent} from 'leaflet';
 import {Map, TileLayer, Marker} from 'react-leaflet';
 import api from '../../services/api';
+import logo from '../../assets/logo.svg'
+import Dropzone from '../../components/Dropzone';
 
 import './styles.css';
 
-import logo from '../../assets/logo.svg'
+interface Props{
+    onFileUploaded:(file:File)=>void;
+}
+
+
 //arry ou objeto: manualmente informar o tipo da variavel
 
 interface Item{
@@ -120,21 +126,27 @@ const CreatePoint = ()=>{
         const city=selectedCity;
         const [latitude, longitude]=selectedPosition;
         const items = selectedItems;
+        const data = new FormData();
 
-        const data={
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
-        await api.post('points',data);
-        alert('Ponto de coleta criado');
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+        
+        if (selectedFile) {
+          data.append('image', selectedFile)
+        }
+    
+        await api.post('points', data);
+    
+        alert('Ponto de coleta criado!');
+    
         history.push('/');
-    }
+      }
 
     return(
         <div id="page-create-point">
@@ -148,6 +160,7 @@ const CreatePoint = ()=>{
 
             <form onSubmit={handleSubbmit}>
             <h1>Cadastro do<br/> ponto de coleta</h1>
+            <Dropzone onFileUploaded={setSelectedFile}/>
             <fieldset>
                 <legend>
                     <h2>Dados</h2>
